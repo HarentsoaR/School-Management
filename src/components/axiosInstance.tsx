b4530367c82fd59658c73be77410485224abdb20
HarentsoaR <randriamaholimanana1@gmail.com>
2024-02-24 14:59:48 +0300

    Server Issue

--- src/components/axiosInstance.tsx
+++ src/components/axiosInstance.tsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create an Axios instance
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8080/api', // Replace with your API base URL
+  baseURL: 'http://192.168.137.1:8080/api', // Replace with your API base URL
 });
 
 // Add a request interceptor
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: '0.0.0.0',
+    // If you're using a custom port, specify it here
+     port:   5173,
+  },
   plugins: [react()],
 })
 
